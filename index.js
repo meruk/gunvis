8cdfceacdc4add9c191c7322cbbdf3c978ceab28
brother <brother@snaps.com>
2018-03-15 16:46:11 +0900

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {ipcRenderer} = require('electron')
 
-ipcRenderer.on('saySomething', (args) => {
+ipcRenderer.on('saySomething', (event, args) => {
+alert(args)
   var synth = window.speechSynthesis;
   var voice = synth.getVoices()[0]
   var utterThis = new SpeechSynthesisUtterance(args);
